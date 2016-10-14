b8f1717c9054fb3d830794ebc27aaab82e4380b4
noopurP <parikh.noopur18@gmail.com>
2016-10-14 10:55:37 -0400

    Update pytree.py

--- pytree.py
+++ pytree.py
@@ -15,37 +15,29 @@ def tree(dir, padding):
     for f in content:
         count = count + 1
         if not f.startswith('.'):
-            if count == len(content):
+            if count != len(content):
+                print(padding + "├── " + f)
+            else:
                 print(padding + "└── " + f)
-                if isdir(os.path.realpath(dir + '/' + f)):
-                    global totdir
-                    totdir = totdir + 1
-                    padding = padding + "   "
-                    padding = tree(os.path.realpath(dir + '/' + f), padding)
-                else:
-                    global totfil
-                    totfil = totfil + 1
+            if isdir(os.path.realpath(dir + '/' + f)):
+                global totdir
+                totdir = totdir + 1
+                padding = padding + "│   "
+                padding = tree(os.path.realpath(dir + '/' + f), padding)
+            else:
+                global totfil
+                totfil = totfil + 1
+            if count == len(content):
                 str = []
                 str = padding.rpartition("│")
                 padding = str[0]
                 return padding
-            else:
-                print(padding + "├── " + f)
-                if isdir(os.path.realpath(dir + '/' + f)):
-                    global totdir
-                    totdir = totdir + 1
-                    padding = padding + "│   "
-                    padding = tree(os.path.realpath(dir + '/' + f), padding)
-                else:
-                    global totfil
-                    totfil = totfil + 1
 
 
 def main():
     if len(sys.argv) == 1:
-        path = os.getcwd()
         print(".")
-        tree(path, '')
+        tree(".", '')
     else:
         path = os.path.realpath(sys.argv[1])
         print(sys.argv[1])
